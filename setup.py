9de0182ce40c3c66f89ecfb633cbf6dcc78503d6
Dylan Jenkinson <dylan@dylan-jenkinson.nz>
2017-01-20 19:43:53 +1300

    Setting up python package structure part 3

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scripts',
+      version='0.1',
+      description='',
+      url='https://github.com/froomzy/scripts',
+      author='Dylan Jenkinson',
+      author_email='dylan@dylan-jenkinson.nz',
+      license='MIT',
+      packages=['scripts'],
+      zip_safe=False)
