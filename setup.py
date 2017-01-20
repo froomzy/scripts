885ab6264ad83c0e591bd4f6b3636a3abf9abdcc
Dylan Jenkinson <dylan@dylan-jenkinson.nz>
2017-01-20 20:10:40 +1300

    Added a basic script that should turn into a command line thingy. We shall see.

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(name='scripts',
       version='0.1',
-      description='',
+      description='A set of scripts frop deploying my apps to their servers,',
       url='https://github.com/froomzy/scripts',
       author='Dylan Jenkinson',
       author_email='dylan@dylan-jenkinson.nz',
       license='MIT',
       packages=['scripts'],
+      install_requires=[
+          'Fabric3'
+      ]
       zip_safe=False)
